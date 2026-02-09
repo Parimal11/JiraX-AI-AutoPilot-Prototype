8619b14a814bc602532359efb35114f2cb62674c
Parimal11 <anshuldeshmukh6323@gmail.com>
2026-02-09 19:17:53 +0530

    Initial Deploy

--- main.py
+++ main.py
@@ -122,4 +122,6 @@ async def analyze_vision(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
